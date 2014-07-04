e633d8ddc26c4f557488d609295451d0cbce9df4
s_murakami <s.murakami@conol.jp>
2014-07-04 11:00:02 +0900

    Questions作成

---
+++ SpajamPre/Questions.h
@@ -0,0 +1,13 @@
+//
+//  Questions.h
+//  SpajamPre
+//
+//  Created by 村上 晋太郎 on 2014/07/04.
+//  Copyright (c) 2014年 村上 晋太郎. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Questions : NSMutableArray
+- (void)load;
+@end
---
+++ SpajamPre/Questions.m
@@ -0,0 +1,16 @@
+//
+//  Questions.m
+//  SpajamPre
+//
+//  Created by 村上 晋太郎 on 2014/07/04.
+//  Copyright (c) 2014年 村上 晋太郎. All rights reserved.
+//
+
+#import "Questions.h"
+
+@implementation Questions
+- (void)load
+{
+  // To Do
+}
+@end
